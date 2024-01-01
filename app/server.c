27c04555936ef78394fb2970e3d3904ee832899b
RANIVO Michael <mickaelpatrickranivo@gmail.com>
2024-01-01 21:05:26 +0100

    pass 5nd stage

--- app/server.c
+++ app/server.c
@@ -7,7 +7,7 @@
 #include <errno.h>
 #include <unistd.h>
 
-#define BUFFER_SIZE	1024
+#define BUFFER_SIZE	2048
 
 char *request_path(char *buffer){
 	// take the first line of the HTTP request
@@ -29,23 +29,6 @@ char *extract_string(char *buffer){
 
 	// return the string on the path
 	return slash_position+1;
-}
-
-int path_valid(char *buffer, ssize_t bytes_read){
-
-	char *get = "GET / HTTP/1.1\r\n";
-	if(bytes_read < strlen(get)){
-		return 0;
-	}
-
-	char *path = request_path(buffer);
-
-	//test if the path is the root path 
-	if(strcmp(path, "/") != 0 ){
-		return 0;
-	}
-
-	return 1;
 }
 
 int main() {
@@ -123,7 +106,6 @@ int main() {
 		printf("Send: %zd \nbytes: \n%s\n", server_send_respond, server_respond);
 
 	}else if(strncmp(path, "/echo/", 6) == 0){ // test if the path contains the substring "/echo/"
-
 		// extract the string we need on the path
 		char *string = extract_string(path);
 		size_t string_len = strlen(string);
@@ -138,8 +120,24 @@ int main() {
 			return 1;	
 		}
 		printf("Send: %zd \nbytes: \n%s\n", server_send_respond, resultat);
-	}else{
 
+	}else if (strncmp(path, "/user-agent", 11) == 0){
+		char *userAgentHttp = strstr(client_response, "User-Agent:");
+    	char *userAgentLine = strndup(userAgentHttp, strstr(userAgentHttp, "\r\n") - userAgentHttp);
+		char *userAgent = strstr(userAgentLine, " ")+1;
+		size_t userAgentLen = strlen(userAgent);
+
+		char resultatUserAgent[BUFFER_SIZE];
+		sprintf(resultatUserAgent, "HTTP/1.1 200 ok\r\nContent-Type: text/plain\r\nContent-Length: %ld\r\n\r\n%s", userAgentLen, userAgent);
+
+		ssize_t server_send_respond = send( client_accepted, resultatUserAgent, strlen(resultatUserAgent), 0);
+		if(server_send_respond == -1){
+			printf("The respond fail to be sended: %s \n", strerror(errno));
+			return 1;	
+		}
+		printf("Send: %zd \nbytes: \n%s\n", server_send_respond, resultatUserAgent);
+
+	}else{
 		char *server_fail_respond = "HTTP/1.1 404 Not Found\r\n\r\n";
 		ssize_t server_send_respond = send( client_accepted, server_fail_respond, strlen(server_fail_respond), 0);
 		if(server_send_respond == -1){
@@ -147,6 +145,7 @@ int main() {
 			return 1;	
 		}
 		printf("Send: %zd \nbytes: \n%s\n", server_send_respond, server_fail_respond);
+
 	}
 		
 	close(server_fd);
